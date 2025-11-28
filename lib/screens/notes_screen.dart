51d39fc844294713b7ffa19f16a60d1fd9c5821b
SharikkkEk <103816162+SharikkkEk@users.noreply.github.com>
2025-11-28 23:38:35 +0400

    полурабочая связь гл экрана с заметками

--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -6,8 +6,8 @@ import 'package:expressive_refresh/expressive_refresh.dart';
 import 'package:flutter/material.dart';
 import 'package:timetable/main.dart';
 import 'package:timetable/models/note.dart';
-import 'package:timetable/screens/schedule_screen.dart';
-import 'package:timetable/widgets/new_lesson_widget.dart';
+import 'package:timetable/models/lesson.dart';
+import 'package:timetable/widgets/note_widgets.dart';
 import 'package:provider/provider.dart';
 
 class NotesScreen extends StatefulWidget {
@@ -34,7 +34,10 @@ class _NotesScreenState extends State<NotesScreen> {
             builder: (context, keys, children) {
               return ListView.builder(
                 itemCount: keys.length,
-                itemBuilder: (_, index) => OpenContainerTest(id: keys[index]),
+                itemBuilder: (_, index) => NoteWidget(
+                  id: keys[index],
+                  closedBuilder: GeneralNoteView(id: keys[index]),
+                ),
               );
             },
           ),
@@ -45,11 +48,8 @@ class _NotesScreenState extends State<NotesScreen> {
           child: TextButton(
             child: Text('+', style: TextStyle(fontSize: 50)),
             onPressed: () {
-              notes.add(
-                LessonID(0, 0, 0, ""),
-                Note('Заметка #${notes.length}', ""),
-              );
-              /*showDialog(
+              /*notes.add(LessonID(0, 0, 0, ""));*/
+              showDialog(
                 context: context,
                 builder: (BuildContext context) {
                   int week = 0;
@@ -90,10 +90,7 @@ class _NotesScreenState extends State<NotesScreen> {
                       ),
                       TextButton(
                         onPressed: () {
-                          notes.add(
-                            LessonID(week, day, index, name),
-                            Note('$name/$week/$day', ""),
-                          );
+                          notes.add(LessonID(week, day, index, name));
                           Navigator.of(context).pop();
                         },
                         child: Text('Принять'),
@@ -101,152 +98,11 @@ class _NotesScreenState extends State<NotesScreen> {
                     ],
                   );
                 },
-              );*/
+              );
             },
           ),
         ),
       ],
-    );
-  }
-}
-
-class TextContainer extends StatelessWidget {
-  final LessonID id;
-  const TextContainer({super.key, required this.id});
-
-  @override
-  Widget build(BuildContext context) {
-    return Selector<LessonNotes, Note?>(
-      selector: (_, provider) => provider.getNote(id),
-      builder: (context, note, child) {
-        return Container(
-          margin: EdgeInsets.all(30),
-          child: Column(
-            children: [
-              TextFormField(
-                style: TextStyle(fontSize: 44, color: Colors.white),
-                initialValue: note?.title ?? "",
-                onChanged: (value) => note?.setTitle(value),
-              ),
-              Expanded(
-                child: TextFormField(
-                  style: TextStyle(color: Colors.white),
-                  initialValue: note?.content ?? "",
-                  onChanged: (value) => note?.setContent(value),
-                  expands: true,
-                  maxLines: null,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Введите текст заметки',
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-}
-
-class OpenContainerTest extends StatelessWidget {
-  final LessonID id;
-  //Color(0x442D9CFF);
-  const OpenContainerTest({super.key, required this.id});
-
-  @override
-  Widget build(BuildContext context) {
-    final Color shapeColor = Theme.of(context).colorScheme.primaryContainer;
-    final model = Provider.of<LessonNotes>(context, listen: false);
-    return Padding(
-      padding: const EdgeInsets.only(left: 8, right: 8, bottom: 4, top: 0),
-      child: OpenContainer(
-        transitionDuration: Duration(milliseconds: 500),
-        closedElevation: 0,
-        closedShape: RoundedRectangleBorder(
-          borderRadius: BorderRadiusGeometry.circular(8),
-        ),
-        closedColor: shapeColor,
-        closedBuilder: (context, action) {
-          return GestureDetector(
-            onTap: () {
-              action();
-            },
-            child: SizedBox(
-              width: double.infinity,
-              child: Padding(
-                padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
-                //child: Material(
-                //borderRadius: BorderRadius.circular(16),
-                //color: fig_col,
-                child: Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                      Selector<LessonNotes, String>(
-                        selector: (_, provider) =>
-                            provider.getNote(id)?.title ?? "",
-                        builder: (context, value, child) {
-                          return Text(
-                            value,
-                            style: TextStyle(color: Colors.white, fontSize: 16),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                //),
-              ),
-            ),
-          );
-        },
-        openBuilder: (context, action) {
-          return Scaffold(
-            appBar: AppBar(),
-            body: Center(
-              child: Hero(
-                tag: "tag",
-                child: Material(
-                  color: shapeColor, //ColorScheme.of(context).surfaceVariant,
-                  child: SizedBox.expand(
-                    child: Center(
-                      child: Stack(
-                        //mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          TextContainer(id: id),
-                          SizedBox(height: 20),
-                          Container(
-                            alignment: Alignment.bottomRight,
-                            margin: EdgeInsets.all(30),
-                            child: ElevatedButton(
-                              onPressed: () {
-                                action();
-                              },
-                              child: Text("Закрыть"),
-                            ),
-                          ),
-                          Container(
-                            alignment: Alignment.bottomLeft,
-                            margin: EdgeInsets.all(30),
-                            child: ElevatedButton(
-                              onPressed: () {
-                                action();
-                                model.delNote(id);
-                              },
-                              child: Text("Удалить"),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          );
-        },
-      ),
     );
   }
 }
